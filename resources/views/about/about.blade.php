2cfd294eac99b666a56d063dfa806ce56e8aae1b
Sirojiddin10 <sirojiddinodamboev@gmail.com>
2021-04-25 23:35:31 +0500

    Sirojiddin Odamboev U1910281

---
+++ resources/views/about/about.blade.php
@@ -0,0 +1,3 @@
+<p style='color:red; font-family:Snell Roundhand, cursive; text-align:center;'>About me</p>
+<h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iure maxime suscipit explicabo quibusdam quidem
+    quaerat incidunt error. Dolores fugit a odio aperiam vitae voluptatem nulla tenetur impedit illo itaque.</h3>
